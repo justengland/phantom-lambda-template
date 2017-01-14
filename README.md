e7793ed027861c95e4bde2bca06984c06707fa1b
jengland <jengland@healthgrades.com>
2017-01-14 10:44:42 -0700

    First commit on v1

--- README.md
+++ README.md
@@ -1,5 +1,44 @@
 # PhantomJS Lambda Template
 
+This is a reference implementation of running [PhantomJS](http://phantomjs.org/) on [AWS Lambda](http://aws.amazon.com/lambda/) with a pipeline.
+
+## Background
+PhantomJS needs to be compiled for the OS you plan on running it. Until now this whole project has been painful because of that fact.
+This could be circumvented with a build server, which is a very personal decision and it was hard to commit to a build server for this little project.
+Now with CodeBuild, this has become a trivial matter. So we are using AWS Developer tools 100%, AWS created a great
+[walk through: Automating Deployment of Lambda-based Applications](http://docs.aws.amazon.com/lambda/latest/dg/automating-deployment.html)
+I have done my best to automate the walk through, so its simple and repeatable.
+
+## Setup
+1. Setup your AWS account and AWS CLI
+1. Fork the project
+1. Setup a personal access token with GitHub for [AWS CodePipeline](https://aws.amazon.com/codepipeline/) , you can skip this if you use [AWS CodeCommit](https://aws.amazon.com/codecommit/)
+https://github.com/settings/tokens
+1. npm install
+1. npm test
+1. ```npm run deploy-pipeline -- \
+            GitHubToken=< your token > \
+            GitHubUser=justengland \
+            Repo=phantom-lambda-template \
+            Branch=master
+    ```
+
+npm run deploy-pipeline -- GitHubToken=6627170ea6aa70139deba4da92eeab0748649cbd GitHubUser=justengland "Repo=phantom-lambda-template" Branch=master --profile daws
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 A [PhantomJS](http://phantomjs.org/) node.js app for [Amazon Lambda](http://aws.amazon.com/lambda/). Based on [node-lambda-template](https://github.com/rebelmail/node-lambda-template) using [node-lambda](https://github.com/rebelmail/node-lambda). The app includes a PhantomJS binary (`phantomjs`) compiled for AWS Linux (https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-1.9.8-linux-x86_64.tar.bz2).
 
 ## Try my new NPM Module!  [phantomjs-lambda-pack](https://github.com/justengland/phantomjs-lambda-pack)
--- index.js
+++ index.js
@@ -1,79 +1,23 @@
-// Entry Point
-exports.handler = function( event, context ) {
-  "use strict";
 
-  var path = require('path'),
-      fs = require('fs'),
-      os = require('os'),
-      http = require('http'),
-      childProcess = require('child_process');
+var AWS = require('aws-sdk');
+var phantomjs = require('phantomjs-prebuilt');
 
-  // Get the path to the phantomjs application
-  function getPhantomFileName(callback) {
-    var nodeModulesPath = path.join(__dirname, 'node_modules', 'phantomjs-prebuilt');
-    fs.exists(nodeModulesPath, function(exists) {
-      if (exists) {
-        callback(path.join(__dirname, 'node_modules','phantomjs-prebuilt', 'bin', 'phantomjs'));
-      }
-      else {
-        callback(path.join(__dirname, 'phantomjs'));
-      }
+exports.handler = function(event, context, callback) {
+    console.log('I am on master');
+    var phantom = phantomjs.exec('phantomjs-script.js', 'arg1', 'arg2');
+
+    phantom.stdout.on('data', function(buf) {
+        console.log('[STR] stdout "%s"', String(buf));
     });
-  }
-
-  // Call the phantomjs script
-  function callPhantom(callback) {
-    getPhantomFileName(function(phantomJsPath) {
-
-      var childArgs = [
-        path.join(__dirname, 'phantomjs-script.js')
-      ];
-
-      // replace default phantomjs script with something the invoker sent
-      if (event && event.code) {
-        childArgs[0] = generateTmpScript()
-      }
-
-      // This option causes the shared library loader to output
-      // useful information if phantomjs can't start.
-      process.env['LD_WARN'] = 'true';
-
-      // Tell the loader to look in this script's directory for
-      // the shared libraries that Phantom.js 2.0.0 needs directly.
-      // This shouldn't be necessary once
-      // https://github.com/ariya/phantomjs/issues/12948
-      // is fixed.
-      process.env['LD_LIBRARY_PATH'] = __dirname;
-
-      console.log('Calling phantom: ', phantomJsPath, childArgs);
-      var ls = childProcess.execFile(phantomJsPath, childArgs);
-
-      ls.stdout.on('data', function (data) {    // register one or more handlers
-        console.log(data);
-      });
-
-      ls.stderr.on('data', function (data) {
-        console.log('phantom error  ---:> ' + data);
-      });
-
-      ls.on('exit', function (code) {
-        console.log('child process exited with code ' + code);
-        callback();
-      });
-
+    phantom.stderr.on('data', function(buf) {
+        console.log('[STR] stderr "%s"', String(buf));
     });
-  }
+    phantom.on('close', function(code) {
+        console.log('[END] code', code);
+    });
 
-  function generateTmpScript() {
-    // when running locally context.json might not contain invokeid
-    var filename = path.join(os.tmpdir(), (context.invokeid || Date.now()) + '.js')
-    fs.writeFileSync(filename, event.code)
+    phantom.on('exit', code => {
+        callback(null, 'fin!!');
+    });
 
-    return filename
-  }
-
-  // Execute the phantom call and exit
-  callPhantom(function() {
-    context.done();
-  });
-}
+};
